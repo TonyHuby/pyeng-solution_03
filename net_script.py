005e3a9d2b67cf6fa8edcb6d4af3d99fd8b725e0
ivanqray <ivan.qray@gmail.com>
2019-08-28 19:37:28 +0300

    5.a - done

--- net_script.py
+++ net_script.py
@@ -9,6 +9,9 @@ addr[3] = mask[0]
 zeros = 32 - int(mask[1])
 binmask = int(mask[1])*'1' + zeros*'0'
 
+#NETWORK MAGIC
+binaddr = '{:08b}'.format(int(addr[0]))+'{:08b}'.format(int(addr[1]))+'{:08b}'.format(int(addr[2]))+'{:08b}'.format(int(addr[3]))
+binout = binaddr[:-zeros] + zeros * '0'
 
 output_n = ['\nNetwork:',
 '{:<8} {:<8} {:<8} {:<8}',
@@ -16,5 +19,6 @@ output_n = ['\nNetwork:',
 
 output_m = ['\nMask:', '/{:8}', '{:<8} {:<8} {:<8} {:<8}', '{:<8} {:<8} {:<8} {:<8}']
 
-print('\n'.join(output_n).format(addr[0],addr[1],addr[2],addr[3],int(addr[0]),int(addr[1]),int(addr[2]),int(addr[3])))
+#print('\n'.join(output_n).format(addr[0],addr[1],addr[2],addr[3],int(addr[0]),int(addr[1]),int(addr[2]),int(addr[3])))
+print('\n'.join(output_n).format(int(binout[:8],2),int(binout[8:16],2),int(binout[16:24],2),int(binout[24:],2),int(binout[:8],2),int(binout[8:16],2),int(binout[16:24],2),int(binout[24:],2)))
 print('\n'.join(output_m).format(mask[1],int(binmask[:8],2),int(binmask[8:16],2),int(binmask[16:24],2),int(binmask[24:],2),binmask[:8],binmask[8:16],binmask[16:24],binmask[24:]))
